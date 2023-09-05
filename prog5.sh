a8759b192b065508370f701d8c3d3d9daa7c874b
dhanush <nnm22is04@nmamit.in>
2023-09-06 01:11:41 +0530

    Lab5

---
+++ prog5.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+if [ $# -ne 1 ]
+then 
+    echo "no arguments "
+    exit
+fi
+cdir=`pwd`
+for dir in `echo $1 | tr '/' ' '`
+do 
+    if [ -d $dir ]
+    then
+        echo "$dir exists under `pwd`"
+        cd $dir
+    else
+        mkdir $dir
+        echo "$dir created under `pwd` "
+        cd $dir
+    fi
+    done
+    cd $cdir
+ss
